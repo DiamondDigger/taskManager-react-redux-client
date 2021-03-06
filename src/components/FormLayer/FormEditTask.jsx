3bfaed5ef005577e1c825e40a50b0ea8ae5d57ba
naglec13 <naglec13@gmail.com>
2021-03-06 19:43:33 +0300

    -#6- add form to edit task text

---
+++ src/components/FormLayer/FormEditTask.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "../styles/form/formAddTask.css";
+
+const FormEditTask = () => {
+  const username = "username";
+  const email = "email";
+
+  const linkToServer = "";
+  return (
+    <form action={linkToServer} method="post">
+      <ul>
+        <li>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="user_name"
+            value={username}
+            disabled
+          />
+        </li>
+        <li>
+          <label htmlFor="mail">E-mail:</label>
+          <input
+            type="email"
+            id="mail"
+            name="user_mail"
+            value={email}
+            disabled
+          />
+        </li>
+        <li>
+          <label htmlFor="msg">Message:</label>
+          <textarea
+            id="msg"
+            name="user_message"
+            cols="30"
+            rows="10"
+            required
+          ></textarea>
+        </li>
+        <li className="button">
+          <button type="submit">Save edited task</button>
+        </li>
+      </ul>
+    </form>
+  );
+};
+
+export default FormEditTask;
