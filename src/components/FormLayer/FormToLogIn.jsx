5daf743f5397471e408f5f331ebbdbea01aaf128
naglec13 <naglec13@gmail.com>
2021-03-06 19:10:41 +0300

    -#5- add form to log in

--- src/components/FormLayer/FormToLogIn.jsx
+++ src/components/FormLayer/FormToLogIn.jsx
@@ -11,21 +11,12 @@ const FormAddTask = () => {
           <input type="text" id="name" name="user_name" required />
         </li>
         <li>
-          <label htmlFor="mail">E-mail:</label>
-          <input type="email" id="mail" name="user_mail" required />
-        </li>
-        <li>
-          <label htmlFor="msg">Message:</label>
-          <textarea
-            id="msg"
-            name="user_message"
-            cols="30"
-            rows="10"
-            required
-          ></textarea>
+          <label htmlFor="mail">Password:</label>
+          <input type="email" id="mail" name="user_password" required />
         </li>
         <li className="button">
-          <button type="submit">Save your task</button>
+          <button type="submit">Log In</button>
+          <button type="button">Cancel</button>
         </li>
       </ul>
     </form>
